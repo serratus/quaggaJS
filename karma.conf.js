ce81e9169ca6b1d567a3b9faea4a3437ede8e3f7
Christoph Oberhofer <ch.oberhofer@gmail.com>
2016-03-28 17:40:47 +0200

    Fixed test-runner

--- karma.conf.js
+++ karma.conf.js
@@ -48,6 +48,7 @@ module.exports = function(config) {
         },
         plugins: [
             'karma-chrome-launcher',
+            'karma-firefox-launcher',
             'karma-coverage',
             'karma-mocha',
             'karma-chai',
